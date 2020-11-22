709db18a7eea84eba80976beb515130f70c15a4f
Algarvio-IG <73707129+Algarvio-IG@users.noreply.github.com>
2020-11-22 22:45:09 +0000

    Tutorial2_3

---
+++ unimage/src/main/java/pt/iade/unimage/Controllers/Response.java
@@ -0,0 +1,18 @@
+package pt.iade.unimage.Controllers;
+
+public class Response {
+    private String message;
+    private Object object;
+    
+    public Response(String message, Object object) {
+        this.message = message;
+        this.object = object;
+    }
+
+    public String getMessage() {
+         return message;
+         }
+    public Object getObject() {
+         return object; 
+        }
+}
